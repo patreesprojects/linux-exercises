37ea0feb921b6315d8f96efefdd81c3928c6d7fa
„Patryk <Taraszewski.patryk@gmail.com>
2025-04-29 06:40:02 -0600

    skrypt na podstawowa petle for, shell scripts

---
+++ task05_shell_scripts/for_exercise.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#petla zaczyna sie od for, czyli "dla" zmiennej w ktorej(in) jest pies kot kon
+#zrob: echo(wypisz/powtorz) slowo zwierze: zmienna zwierze
+#done konczy petle
+for zwierze in pies kot kon
+	do
+	echo "Zwierze: $zwierze"
+done
